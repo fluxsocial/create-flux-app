162f7cd6e8fcb86625ec649719a3c67576d7abdf
Bård N. Hovde <bnhovde@gmail.com>
2023-03-21 17:43:52 +0100

    Added new logic for showing perspective

--- templates/preact/src/App.tsx
+++ templates/preact/src/App.tsx
@@ -1,10 +1,22 @@
-import AllNeighbourhoods from "./components/AllNeighbourhoods";
-import styles from "./App.module.css";
+import { useEffect } from "preact/hooks";
+import { PerspectiveProxy } from "@perspect3vism/ad4m";
+import Perspective from "./components/Perspective";
 
-export default function App({ perspective, source }) {
+import "./styles.css";
+
+type Props = {
+  perspective: PerspectiveProxy;
+};
+
+export default function App({ perspective }: Props) {
+  // Import Junto elements
+  // package.json info
+  // picture, name
+  // keyword flux-app, ad4m-widget
+
   return (
-    <div className={styles.appContainer}>
-      <AllNeighbourhoods />
+    <div className="appContainer">
+      <Perspective perspective={perspective} />
     </div>
   );
 }
---
+++ templates/preact/src/components/Perspective.tsx
@@ -0,0 +1,72 @@
+import { PerspectiveProxy } from "@perspect3vism/ad4m";
+import { useEffect, useState } from "preact/hooks";
+
+type Props = {
+  perspective: PerspectiveProxy;
+};
+
+export default function Perspective({ perspective }: Props) {
+  const [subjects, setSubjects] = useState([]);
+
+  async function fetchneighbourhoods() {
+    const subjectClasses = await perspective.subjectClasses();
+    setSubjects([...new Set(subjectClasses)]);
+  }
+
+  useEffect(() => {
+    if (perspective) {
+      fetchneighbourhoods();
+    }
+  }, [perspective]);
+
+  return (
+    <div>
+      <j-text variant="heading-lg">My First Flux App</j-text>
+      <j-text variant="body">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore
+        nostrum placeat ea illo cumque sunt repellat eos! Quo omnis,
+        perspiciatis corporis recusandae cum culpa reiciendis aliquam unde vero
+        pariatur praesentium!
+      </j-text>
+
+      <j-box pt={400}>
+        <j-text variant="heading-sm">Selected perspective:</j-text>
+      </j-box>
+
+      <div className="perspective">
+        {perspective ? (
+          <>
+            <j-text variant="label">Name</j-text>
+
+            <j-text variant="body" color="black">
+              {perspective.name || "Untitled perspective"}
+            </j-text>
+
+            <j-text variant="label">Uuid</j-text>
+            <j-text variant="body" color="black">
+              {perspective.uuid}
+            </j-text>
+
+            <j-text variant="label">Shared URL</j-text>
+            <j-text variant="body" color="black">
+              {perspective.sharedUrl}
+            </j-text>
+
+            <j-text variant="label">Subject classes:</j-text>
+            <ul>
+              {subjects?.map((s) => (
+                <li key={s}>{s}</li>
+              ))}
+            </ul>
+          </>
+        ) : (
+          <>
+            <j-text variant="body" nomargin>
+              Select a perspective or create a new one
+            </j-text>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
